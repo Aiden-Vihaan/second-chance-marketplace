f44020e28d6f5b48556f9e904b46d8ed07279c0e
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 21:47:48 +0530

    Update Wishlist.js

--- backend/src/models/Wishlist.js
+++ backend/src/models/Wishlist.js
@@ -1,1 +1,26 @@
+import mongoose from "mongoose";
 
+const wishlistSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    items: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Item"
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.model(
+  "Wishlist",
+  wishlistSchema
+);
