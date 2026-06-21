47a340b121139174d6ab57e7f333b8f38a623d99
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 21:56:30 +0530

    Update wishlistRoutes.js

--- backend/src/routes/wishlistRoutes.js
+++ backend/src/routes/wishlistRoutes.js
@@ -1,1 +1,31 @@
+import express from "express";
 
+import {
+  addToWishlist,
+  removeFromWishlist,
+  getWishlist
+} from "../controllers/wishlistController.js";
+
+import protect from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get(
+  "/",
+  protect,
+  getWishlist
+);
+
+router.post(
+  "/:itemId",
+  protect,
+  addToWishlist
+);
+
+router.delete(
+  "/:itemId",
+  protect,
+  removeFromWishlist
+);
+
+export default router;
