70460a6081ecb48e474d8ee2d4c0a673f8191873
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 21:16:56 +0530

    Create wishlist.test.js

---
+++ backend/tests/wishlist.test.js
@@ -0,0 +1,9 @@
+describe("Wishlist", () => {
+  test("Add to wishlist endpoint exists", () => {
+    expect(true).toBe(true);
+  });
+
+  test("Remove from wishlist endpoint exists", () => {
+    expect(true).toBe(true);
+  });
+});
