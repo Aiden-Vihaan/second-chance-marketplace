4c4be0330d79e27435603d3908aa3c7544788788
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 22:18:31 +0530

    Update reviewValidator.js

--- backend/src/validators/reviewValidator.js
+++ backend/src/validators/reviewValidator.js
@@ -1,1 +1,24 @@
+export const validateReview = (
+  rating,
+  comment
+) => {
+  if (!rating || !comment) {
+    return {
+      valid: false,
+      message:
+        "Rating and comment required"
+    };
+  }
 
+  if (rating < 1 || rating > 5) {
+    return {
+      valid: false,
+      message:
+        "Rating must be between 1 and 5"
+    };
+  }
+
+  return {
+    valid: true
+  };
+};
