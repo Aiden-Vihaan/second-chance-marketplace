db9f626b3c5b17ff7775bf190c48d0201abc5077
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 21:47:05 +0530

    Update Review.js

--- backend/src/models/Review.js
+++ backend/src/models/Review.js
@@ -1,1 +1,37 @@
+import mongoose from "mongoose";
 
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    item: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item",
+      required: true
+    },
+
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true
+    },
+
+    comment: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.model(
+  "Review",
+  reviewSchema
+);
