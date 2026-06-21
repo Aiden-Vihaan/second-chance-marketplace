b3907707663fd7f8c4d6f4860bb666c2380ef4fe
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 20:54:05 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,1 +1,20 @@
+import { Link } from "react-router-dom";
 
+function Navbar() {
+  return (
+    <nav className="container">
+      <h2>Second Chance</h2>
+
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/marketplace">Marketplace</Link>
+        <Link to="/add-item">Sell Item</Link>
+        <Link to="/wishlist">Wishlist</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
