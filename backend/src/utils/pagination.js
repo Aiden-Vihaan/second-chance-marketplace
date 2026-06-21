982357e450c1423496d278ebd5e2a4de1734416e
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 22:13:31 +0530

    Update pagination.js

--- backend/src/utils/pagination.js
+++ backend/src/utils/pagination.js
@@ -1,1 +1,14 @@
+const getPagination = (
+  page = 1,
+  limit = 10
+) => {
+  const skip =
+    (page - 1) * limit;
 
+  return {
+    limit: Number(limit),
+    skip: Number(skip)
+  };
+};
+
+export default getPagination;
