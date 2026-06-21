81791da36ed78493b4f9f7562f9a6e1188c7f0f7
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 21:03:56 +0530

    Update reviewApi.js

--- frontend/src/api/reviewApi.js
+++ frontend/src/api/reviewApi.js
@@ -1,1 +1,22 @@
+import apiClient from "./apiClient";
 
+export const getReviews = async (
+  itemId
+) => {
+  const response = await apiClient.get(
+    `/reviews/${itemId}`
+  );
+
+  return response.data;
+};
+
+export const createReview = async (
+  reviewData
+) => {
+  const response = await apiClient.post(
+    "/reviews",
+    reviewData
+  );
+
+  return response.data;
+};
