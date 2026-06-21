330a62371fb9a503197a7a3400912237543ed55e
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 21:52:03 +0530

    Update itemRoutes.js

--- backend/src/routes/itemRoutes.js
+++ backend/src/routes/itemRoutes.js
@@ -1,1 +1,40 @@
+import express from "express";
 
+import {
+  createItem,
+  getItems,
+  getItemById,
+  updateItem,
+  deleteItem
+} from "../controllers/itemController.js";
+
+import protect from "../middleware/authMiddleware.js";
+
+import upload from "../middleware/uploadMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", getItems);
+
+router.get("/:id", getItemById);
+
+router.post(
+  "/",
+  protect,
+  upload.single("image"),
+  createItem
+);
+
+router.put(
+  "/:id",
+  protect,
+  updateItem
+);
+
+router.delete(
+  "/:id",
+  protect,
+  deleteItem
+);
+
+export default router;
