199e2ff96cdf47bf2658d2abbe18a2d0f92b0002
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 22:23:15 +0530

    Create index.js

---
+++ backend/src/validators/index.js
@@ -0,0 +1,5 @@
+export * from "./authValidator.js";
+export * from "./itemValidator.js";
+export * from "./reviewValidator.js";
+export * from "./wishlistValidator.js";
+export * from "./searchValidator.js";
