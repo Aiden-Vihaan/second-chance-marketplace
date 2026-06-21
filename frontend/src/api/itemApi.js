25068d4afc9ebb1de7fc82b344ea3e32f829d6c2
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 21:01:45 +0530

    Update itemApi.js

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,1 +1,36 @@
+import apiClient from "./apiClient";
 
+export const getItems = async () => {
+  const response = await apiClient.get(
+    "/items"
+  );
+
+  return response.data;
+};
+
+export const getItemById = async (id) => {
+  const response = await apiClient.get(
+    `/items/${id}`
+  );
+
+  return response.data;
+};
+
+export const createItem = async (
+  itemData
+) => {
+  const response = await apiClient.post(
+    "/items",
+    itemData
+  );
+
+  return response.data;
+};
+
+export const deleteItem = async (id) => {
+  const response = await apiClient.delete(
+    `/items/${id}`
+  );
+
+  return response.data;
+};
