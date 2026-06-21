7f09925bff9703ef9fb3533f0eecb7fb09924861
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 20:55:49 +0530

    Update SearchBar.jsx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
+function SearchBar({
+  searchTerm,
+  setSearchTerm,
+}) {
+  return (
+    <input
+      type="text"
+      placeholder="Search items..."
+      value={searchTerm}
+      onChange={(e) =>
+        setSearchTerm(e.target.value)
+      }
+    />
+  );
+}
 
+export default SearchBar;
