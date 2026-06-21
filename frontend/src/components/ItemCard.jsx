a4ec0a7c36a70ecec1b8abd4265f536d1bdd6c30
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 20:55:16 +0530

    Update ItemCard.jsx

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -1,1 +1,24 @@
+import { Link } from "react-router-dom";
 
+function ItemCard({ item }) {
+  return (
+    <div>
+      <img
+        src={item.image}
+        alt={item.title}
+      />
+
+      <h3>{item.title}</h3>
+
+      <p>₹{item.price}</p>
+
+      <p>{item.condition}</p>
+
+      <Link to={`/item/${item.id}`}>
+        View Details
+      </Link>
+    </div>
+  );
+}
+
+export default ItemCard;
