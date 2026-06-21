df9b98f6fe0a251876353cc2b93144b37fcc14ac
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 21:15:52 +0530

    Update items.test.js

--- backend/tests/items.test.js
+++ backend/tests/items.test.js
@@ -1,1 +1,9 @@
+describe("Marketplace Items", () => {
+  test("Create item endpoint exists", () => {
+    expect(true).toBe(true);
+  });
 
+  test("Get items endpoint exists", () => {
+    expect(true).toBe(true);
+  });
+});
