1c05565244f3ad348f7696eb12458cec551fa3f5
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 20:54:41 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,1 +1,12 @@
+function Footer() {
+  return (
+    <footer className="container">
+      <p>
+        ©️ 2026 Second Chance Marketplace.
+        All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
 
+export default Footer;
