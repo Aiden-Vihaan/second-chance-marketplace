6e8d1c3819efd5dacc969ef915d613d06e0c007b
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 22:12:50 +0530

    Update constants.js

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -1,1 +1,13 @@
+export const USER_ROLES = {
+  USER: "user",
+  ADMIN: "admin"
+};
 
+export const ITEM_CONDITIONS = [
+  "New",
+  "Like New",
+  "Good",
+  "Fair"
+];
+
+export const PAGE_SIZE = 10;
