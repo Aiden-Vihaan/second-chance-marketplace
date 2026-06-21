e1fa4b0141e754b70c9b2f03990c111edd5d557d
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 22:06:55 +0530

    Create index.js

---
+++ backend/src/services/index.js
@@ -0,0 +1,7 @@
+export * from "./authService.js";
+export * from "./itemService.js";
+export * from "./reviewService.js";
+export * from "./wishlistService.js";
+export * from "./searchService.js";
+export * from "./emailService.js";
+export * from "./sentimentService.js";
