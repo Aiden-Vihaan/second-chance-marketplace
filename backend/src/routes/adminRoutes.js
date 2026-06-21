471e0d8c37e07fee178d53b7adf27a0761cf1744
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 21:58:09 +0530

    Update adminRoutes.js

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,1 +1,36 @@
+import express from "express";
 
+import {
+  getDashboardStats,
+  getAllUsers,
+  deleteUser
+} from "../controllers/adminController.js";
+
+import protect from "../middleware/authMiddleware.js";
+
+import adminOnly from "../middleware/adminMiddleware.js";
+
+const router = express.Router();
+
+router.get(
+  "/dashboard",
+  protect,
+  adminOnly,
+  getDashboardStats
+);
+
+router.get(
+  "/users",
+  protect,
+  adminOnly,
+  getAllUsers
+);
+
+router.delete(
+  "/users/:id",
+  protect,
+  adminOnly,
+  deleteUser
+);
+
+export default router;
