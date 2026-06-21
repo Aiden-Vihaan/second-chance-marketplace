d57152096031f66bf4b1409c0ce5a4c8cb9f7e12
Aidycodes <aidenvihaan333@gmail.com>
2026-06-21 21:52:46 +0530

    Update reviewRoutes.js

--- backend/src/routes/reviewRoutes.js
+++ backend/src/routes/reviewRoutes.js
@@ -1,1 +1,23 @@
+import express from "express";
 
+import {
+  createReview,
+  getReviews
+} from "../controllers/reviewController.js";
+
+import protect from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post(
+  "/",
+  protect,
+  createReview
+);
+
+router.get(
+  "/:itemId",
+  getReviews
+);
+
+export default router;
