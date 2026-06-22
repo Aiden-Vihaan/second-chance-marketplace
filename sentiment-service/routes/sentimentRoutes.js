ceb0a313265519c2191e42a4150ae1b2b74d406b
Aidycodes <aidenvihaan333@gmail.com>
2026-06-22 08:14:20 +0530

    Update sentimentRoutes.js

--- sentiment-service/routes/sentimentRoutes.js
+++ sentiment-service/routes/sentimentRoutes.js
@@ -1,1 +1,14 @@
+import express from "express";
 
+import {
+  analyzeText
+} from "../controllers/sentimentController.js";
+
+const router = express.Router();
+
+router.post(
+  "/analyze",
+  analyzeText
+);
+
+export default router;
