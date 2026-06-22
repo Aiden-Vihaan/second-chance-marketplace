dcd0b704b3fc00967cd477310d9c26ab35f029f3
Aidycodes <aidenvihaan333@gmail.com>
2026-06-22 08:12:06 +0530

    Update sentimentAnalyzer.js

--- sentiment-service/services/sentimentAnalyzer.js
+++ sentiment-service/services/sentimentAnalyzer.js
@@ -1,1 +1,34 @@
+import Sentiment from "sentiment";
 
+import {
+  classifySentiment,
+  buildSentimentResponse,
+  sanitizeText
+} from "../utils/sentimentHelpers.js";
+
+const sentiment = new Sentiment();
+
+export const analyzeSentiment = (
+  text
+) => {
+  const cleanedText =
+    sanitizeText(text);
+
+  const result =
+    sentiment.analyze(
+      cleanedText
+    );
+
+  const sentimentLabel =
+    classifySentiment(
+      result.score
+    );
+
+  return buildSentimentResponse(
+    cleanedText,
+    result.score,
+    sentimentLabel
+  );
+};
+
+export default analyzeSentiment;
