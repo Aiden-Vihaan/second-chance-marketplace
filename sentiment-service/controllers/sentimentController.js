f47609aa6a364237fa12e826c1967119ada2f72b
Aidycodes <aidenvihaan333@gmail.com>
2026-06-22 08:13:15 +0530

    Update sentimentController.js

--- sentiment-service/controllers/sentimentController.js
+++ sentiment-service/controllers/sentimentController.js
@@ -1,1 +1,31 @@
+import analyzeSentiment from "../services/sentimentAnalyzer.js";
 
+export const analyzeText = async (
+  req,
+  res,
+  next
+) => {
+  try {
+    const { text } = req.body;
+
+    if (!text) {
+      return res.status(400).json({
+        success: false,
+        message: "Text is required"
+      });
+    }
+
+    const result =
+      analyzeSentiment(text);
+
+    return res.status(200).json(
+      result
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  analyzeText
+};
